7ac761de75a46e762b9bef76bd24ab9aa447cf4a
El Idrissi Lina <lina.elidrissi.etu@b12p7.fil.univ-lille.fr>
2025-02-04 17:00:51 +0100

    début tp

--- src/components/pictures.tsx
+++ src/components/pictures.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styled from 'styled-components';
+import { picturesSelector } from '../reducer';
 
 const Container = styled.div`
   padding: 1rem;
@@ -17,8 +19,16 @@ const Image = styled.img`
     transform: scale(1.2);
   }
 `;
+
 const Pictures = () => {
-  return null;
+  const pictures = useSelector(picturesSelector);
+  return (
+    <Container>
+      {pictures.map(picture => (
+        <Image key={picture.previewFormat} src={picture.previewFormat} alt={picture.author} />
+      ))}
+    </Container>
+  );
 };
 
 export default Pictures;
